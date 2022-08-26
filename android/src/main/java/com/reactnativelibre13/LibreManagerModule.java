3933a899fc54185ebafc7dd558a5cd5ce3ba1343
Ale Imperatori <alejandroimperatori@gmail.com>
2022-08-26 00:05:48 -0300

    Adds freestyle device expiration date

--- android/src/main/java/com/reactnativelibre13/LibreManagerModule.java
+++ android/src/main/java/com/reactnativelibre13/LibreManagerModule.java
@@ -1,34 +1,13 @@
 package com.reactnativelibre13;
 
-import static com.reactnativelibre13.JSONParser.convertJsonToMap;
-
-import androidx.annotation.NonNull;
-
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.WritableNativeArray;
-import com.facebook.react.bridge.WritableNativeMap;
-import com.facebook.react.module.annotations.ReactModule;
-import com.facebook.react.uimanager.IllegalViewOperationException;
-
-import org.json.JSONArray;
+import com.facebook.react.bridge.*;
+import kotlin.Pair;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
-import kotlin.Pair;
+import static com.reactnativelibre13.JSONParser.convertJsonToMap;
 
 public class LibreManagerModule extends ReactContextBaseJavaModule {
     public static final String NAME = "LibreManager";
@@ -66,12 +45,17 @@ public class LibreManagerModule extends ReactContextBaseJavaModule {
         }else {
           data = NFCReader.Companion.append(Objects.requireNonNull(RawParser.Companion.readableArrayToByteStringArray(memoryData)), now, tagId);
         }
+
         List<GlucoseReading> recent = data.getFirst();
         List<GlucoseReading> history = data.getSecond();
         response.putArray("trend_history", getGlucoseReadingsAsWritableArray(recent));
         response.putArray("history", getGlucoseReadingsAsWritableArray(history));
         response.putInt("current_glucose", recent.get(0).getValue());
 
+        WritableMap freestyleDevice = new WritableNativeMap();
+        freestyleDevice.putBoolean("expired", Time.Companion.timeLeft(timestamp).getSecond());
+        freestyleDevice.putDouble("expires_on", Time.Companion.timeLeft(timestamp).getFirst());
+        response.putMap("freestyle_device", freestyleDevice);
 
         promise.resolve(response);
 
