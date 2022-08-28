3d673f094b639b6192e42b99e917c151053328e7
Ale Imperatori <alejandroimperatori@gmail.com>
2022-08-27 23:00:37 -0300

    fix

--- android/src/main/java/com/reactnativelibre13/Time.kt
+++ android/src/main/java/com/reactnativelibre13/Time.kt
@@ -2,15 +2,13 @@ package com.reactnativelibre13
 
 class Time {
   companion object {
-    val SECOND = 1000L
-    val MINUTE = 60L*SECOND
     val DURATION_MINUTES = 20160
     var MINUTES_DAY = 1440.0
     fun now() : Long = System.currentTimeMillis()
 
     fun timeLeft(timeStamp: Int) : Double {
       val left = DURATION_MINUTES - timeStamp
-      if left < 0 {
+      if (left < 0) {
         return 0
       }
 
