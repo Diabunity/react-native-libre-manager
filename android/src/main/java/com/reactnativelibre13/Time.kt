a1bc83c866d216ce29c2271020f59b5268b382ba
Ale Imperatori <alejandroimperatori@gmail.com>
2022-08-27 23:34:25 -0300

    readded constants

--- android/src/main/java/com/reactnativelibre13/Time.kt
+++ android/src/main/java/com/reactnativelibre13/Time.kt
@@ -2,6 +2,8 @@ package com.reactnativelibre13
 
 class Time {
   companion object {
+    val SECOND = 1000L
+    val MINUTE = 60L*SECOND
     val DURATION_MINUTES = 20160
     var MINUTES_DAY = 1440.0
     fun now() : Long = System.currentTimeMillis()
